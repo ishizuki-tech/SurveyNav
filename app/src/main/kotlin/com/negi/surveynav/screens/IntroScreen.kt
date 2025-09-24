8533158d3bb5e38b019c20ac4a9151e86d5f0167
shu <ishizuki.shu@gmail.com>
2025-09-24 12:10:02 -0700

    Clean up and added tests.

--- app/src/main/kotlin/com/negi/surveynav/screens/IntroScreen.kt
+++ app/src/main/kotlin/com/negi/surveynav/screens/IntroScreen.kt
@@ -6,18 +6,24 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun IntroScreen(onStart: () -> Unit) {
-    Scaffold { pad ->
+
+    Scaffold(containerColor = Color.Transparent) { pad ->
         Column(
             Modifier.padding(pad).padding(20.dp).fillMaxSize(),
             verticalArrangement = Arrangement.Center,
